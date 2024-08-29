86ef1423dc9023a0c2b5ff7b2eaf2173c5abf5ee
Alejandra Barros <alejandrabarros.tds1.senai@gmail.com>
2024-08-29 15:51:28 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,9 +8,55 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.5,
+    },
+    {
+        id: 2,
+        nome: "Brigadeiro",
+        preco: 2.5,
+    },
+    {
+        id: 3,
+        nome: "Carolina",
+        preco: 3,
+    }
+]
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "A Mulher Rei",
+        genero: "Ação/Aventura",
+        emCartaz: false,
+    },
+    {
+        id: 1002,
+        titulo: "Mamma Mia",
+        genero: "Musical/Comédia",
+        emCartaz: false,
+    },
+    {
+        id: 1003,
+        titulo: "Hotel Transilvânia",
+        genero: "Infantil/Comédia",
+        emCartaz: false,
+    }
+]
 
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes)
 })
 
 app.listen(port, () => {
